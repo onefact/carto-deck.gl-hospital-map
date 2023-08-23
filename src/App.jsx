3c9771241338a2c063f1c8e8fe8faee8aca2db25
Jaan Altosaar <jaan@onefact.org>
2023-08-23 16:43:50 -0400

    update with sample website with vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Carto from './carto-sql/app.jsx'
+import Test from './carto-sql/app.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,7 +28,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <Carto />
+      <Test />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
